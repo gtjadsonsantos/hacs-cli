c16c38b432374acb9c3caba0bf08a306b2ad52ea
jadson179 <jadson44.santos@gmail.com>
2021-03-27 18:56:04 -0300

    initial commit

---
+++ hacs/constrollers/__init__.py
@@ -0,0 +1,9 @@
+import argparse
+
+class IBasic:
+    def add (self,args:argparse.Namespace) -> None:
+        pass
+    def remove (self,args:argparse.Namespace) -> None:
+        pass
+    def list (self,args:argparse.Namespace) -> None:
+        pass
